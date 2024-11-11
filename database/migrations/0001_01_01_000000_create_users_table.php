48640c91329e3288ad2efc9c356f6c71eeb14aef
AsifRiseofPower <aloasif31@gmail.com>
2024-11-11 13:51:42 +0530

    faker userd for UserProfile

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id('user_id');
-            $table->string('name',20);
+            $table->string('name',50);
             $table->string('email',255)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->text('address', 255);
